a75f0ea95fd3d043545d571663e637731fa5a35a
Ruben Astudillo <ruben.astud@gmail.com>
2016-10-03 02:38:48 -0300

    nuevos valores de lambda para todos los casos
    tenemos formatting en el codigo ahora. correr con octave --no-gui es
    sencillo para esta pregunta

--- pregunta_1.m
+++ pregunta_1.m
@@ -1,30 +1,46 @@
-primer_ej = @(x,y) -10 + x^2 + y^2;
-grad_primer_ej = { @(x,y) 2*x, @(x,y) 2*y };
+# idealmente correr con octave --no-gui pregunta_1.m
+x0  = [1.5, 2.5];
+tol = 10000
+
+primer_ej             = @(x,y) -10 + x^2 + y^2;
+grad_primer_ej        = { @(x,y) 2*x, @(x,y) 2*y };
 inv_hessian_primer_ej = { @(x,y) 0.5, @(x,y) 0; @(x,y) 0, @(x,y) 0.5 };
+printSpec = 'alpha = %.2f, iteraciones = %5d, final = (%.2f, %.2f)';
 
-## En maximo descenso, notamos que para alpha=1 diverge, para alpha < 1
-## converge eventualmente
-[ iter, x1 ] = max_descent(10000, 0.9, grad_primer_ej, [1.5, 2.5])
-[ iter, x1 ] = max_descent(10000, 1, grad_primer_ej, [1.5, 2.5])
+sprintf('Maximo descenso ejemplo1: x0 = (%.2f, %.2f)', x0(1), x0(2))
+for alpha = [0.1 0.2 0.3 0.5 0.7 0.75 0.8 0.85 0.9 0.95 0.98 1 1.2]
+  [ iter, x1 ] = max_descent(tol, alpha, grad_primer_ej, x0);
+  sprintf(printSpec, alpha, iter, x1(1), x1(2))
+end
+sprintf('ultimo caso hay divergencia. Convergencia si alpha < 1 \n')
+
 
 ## En newton con alpha < 2 converge y alpha=2 diverge
-[ iter, x1 ] = newton(10000, 1.99, inv_hessian_primer_ej, ...
-                      grad_primer_ej, [1.5, 2.5])
-[ iter, x1 ] = newton(10000, 2, inv_hessian_primer_ej, ...
-                      grad_primer_ej, [1.5, 2.5])
+sprintf('newton ejemplo1: x0 = (%.2f, %.2f)', x0(1), x0(2))
+for alpha = [1 1.2 1.4 1.6 1.8 1.90 1.95 1.99 2 2.1]
+  [ iter, x1 ] = newton(tol, alpha, inv_hessian_primer_ej, ...
+                        grad_primer_ej, x0);
+  sprintf(printSpec, alpha, iter, x1(1), x1(2))
+end
+sprintf('ultimo caso hay divergencia. Convergencia si alpha < 2 \n')
 
-segundo_ej = @(x,y) -10 + 3*x^2 + y^2;
-grad_segundo_ej = { @(x,y) 6*x, @(x,y) 2*y };
+# ejemplo 2
+segundo_ej             = @(x,y) -10 + 3*x^2 + y^2;
+grad_segundo_ej        = { @(x,y) 6*x, @(x,y) 2*y };
 inv_hessian_segundo_ej = { @(x,y) (1/6), @(x,y) 0; @(x,y) 0, @(x,y) 0.5 };
+printSpecAlt = 'alpha = %.5f, iteraciones = %5d, final = (%.2f, %.2f)';
 
-## En maximo descenso, notamos que para alpha=0.34 diverge, para alpha < 0.34
-## converge eventualmente. Si superamos este valor diverge pero no en
-## cantidad de operaciones si no numericamente, resultando en NaN
-[ iter, x1 ] = max_descent(10000, 0.33, grad_segundo_ej, [1.5, 2.5])
-[ iter, x1 ] = max_descent(10000, 0.34, grad_segundo_ej, [1.5, 2.5])
+sprintf('maximo descenso ejemplo2: x0 = (%.2f, %.2f)', x0(1), x0(2))
+for alpha = [0.2 0.25 0.3 0.32 0.33 (1/3) 0.35]
+  [ iter, x1 ] = max_descent(tol, alpha, grad_segundo_ej, x0);
+  sprintf(printSpecAlt, alpha, iter, x1(1), x1(2))
+end
+sprintf('ultimo caso hay divergencia. Convergencia si alpha < (1/3) \n')
 
-## En newton con alpha < 2 converge y alpha=2 diverge
-[ iter, x1 ] = newton(10000, 1.99, inv_hessian_segundo_ej, ...
-                      grad_segundo_ej, [1.5, 2.5])
-[ iter, x1 ] = newton(10000, 2, inv_hessian_segundo_ej, ...
-                      grad_segundo_ej, [1.5, 2.5])
+sprintf('newton ejemplo2: x0 = (%.2f, %.2f)', x0(1), x0(2))
+for alpha = [1.4 1.6 1.8 1.9 1.95 1.98 1.99 2 2.1]
+  [ iter, x1 ] = newton(tol, alpha, inv_hessian_segundo_ej, ...
+                        grad_segundo_ej, x0);
+  sprintf(printSpec, alpha, iter, x1(1), x1(2))
+end
+sprintf('ultimo caso hay divergencia. Convergencia si alpha < 2 \n')
